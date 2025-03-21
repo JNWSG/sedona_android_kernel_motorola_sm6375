47adbb80c90b59a0a06a3b5ce4bfa211c76d7f8d
Notkerd69 <29781314+Notkerd69@users.noreply.github.com>
2025-03-21 09:55:11 +0900

    treewide: Resolve more clang 18 warnings across multiple drivers

--- drivers/soc/qcom/spss_utils.c
+++ drivers/soc/qcom/spss_utils.c
@@ -557,7 +557,9 @@ static long spss_utils_ioctl(struct file *file,
 			return -EINVAL;
 		}
 		ret = spss_wait_for_event(req);
-		copy_to_user((void __user *)arg, data, size);
+		if (copy_to_user((void __user *)arg, data, size)) {
+    	return -EFAULT;
+		}
 		if (ret < 0)
 			return ret;
 		break;
@@ -569,7 +571,9 @@ static long spss_utils_ioctl(struct file *file,
 			return -EINVAL;
 		}
 		ret = spss_signal_event(req);
-		copy_to_user((void __user *)arg, data, size);
+		if (copy_to_user((void __user *)arg, data, size)) {
+	    return -EFAULT;
+		}
 		if (ret < 0)
 			return ret;
 		break;
@@ -581,7 +585,9 @@ static long spss_utils_ioctl(struct file *file,
 			return -EINVAL;
 		}
 		ret = spss_is_event_signaled(req);
-		copy_to_user((void __user *)arg, data, size);
+		if (copy_to_user((void __user *)arg, data, size)) {
+	    return -EFAULT;
+		}
 		if (ret < 0)
 			return ret;
 		break;
