zetas-lab/android_kernel_motorola_rhodep

disp: msm: Implement backlight re-mapping for rhodep
LUT values generated by cubic spline interpolation of control points
extracted from stock (rhodep) brightness overlays, namely:

config_screenBrightnessBacklight -> config_screenExponentBacklight

Change-Id: Ib741f312bec250700d83bef6aac26c5de4184140
Signed-off-by: Sevenrock <sevenrock@hotmail.de>

techpack/display/msm/sde/sde_connector.c
@@ -21,6 +21,8 @@
 #include "sde_vm.h"
 #include <drm/drm_probe_helper.h>
 
+#include "bl_lut.h"
+
 #define BL_NODE_NAME_SIZE 32
 #define HDR10_PLUS_VSIF_TYPE_CODE      0x81
 
@@ -120,7 +122,7 @@ static int sde_backlight_device_update_status(struct backlight_device *bd)
 	if (brightness > c_conn->thermal_max_brightness)
 		brightness = c_conn->thermal_max_brightness;
 
-	if(brightness && brightness < display->panel->bl_config.bl_min_level)
+	if (brightness && brightness < display->panel->bl_config.bl_min_level)
 		brightness = display->panel->bl_config.bl_min_level;
 
 	/* map UI brightness into driver backlight level with rounding */
@@ -129,6 +131,11 @@ static int sde_backlight_device_update_status(struct backlight_device *bd)
 
 	if (!bl_lvl && brightness)
 		bl_lvl = 1;
+
+	if (display->panel->bl_config.bl_remap) {
+		pr_debug("%s: remap bl_lvl from %d to %d", __func__, bl_lvl, bl_lut[bl_lvl]);
+		bl_lvl = bl_lut[bl_lvl];
+	}
 
 	if (!c_conn->allow_bl_update) {
 		c_conn->unset_bl_level = bl_lvl;
@@ -164,7 +171,9 @@ done:
 
 static int sde_backlight_device_get_brightness(struct backlight_device *bd)
 {
-	return 0;
+	struct sde_connector *c_conn = bl_get_data(bd);
+	struct dsi_display *display = (struct dsi_display *) c_conn->display;
+	return display->panel->bl_config.real_bl_level;
 }
 
 static const struct backlight_ops sde_backlight_device_ops = {

JNWSG/sedona_android_kernel_motorola_sm6375

techpack/display/msm/sde/sde_connector.c
@@ -22,6 +22,8 @@
 #include "sde_vm.h"
 #include <drm/drm_probe_helper.h>
 
+#include "bl_lut.h"
+
 #define BL_NODE_NAME_SIZE 32
 #define HDR10_PLUS_VSIF_TYPE_CODE      0x81
 #define MAX_BRIGHTNESS_LEVEL 255
@@ -147,6 +149,11 @@ static int sde_backlight_device_update_status(struct backlight_device *bd)
 	if (!bl_lvl && brightness)
 		bl_lvl = 1;
 
+	if (dsi_display->panel->bl_config.bl_remap) {
+		pr_debug("%s: remap bl_lvl from %d to %d", __func__, bl_lvl, bl_lut[bl_lvl]);
+		bl_lvl = bl_lut[bl_lvl];
+	}
+
 	if (!c_conn->allow_bl_update) {
 		c_conn->unset_bl_level = bl_lvl;
 		return 0;
@@ -181,7 +188,9 @@ done:
 
 static int sde_backlight_device_get_brightness(struct backlight_device *bd)
 {
-	return 0;
+	struct sde_connector *c_conn = bl_get_data(bd);
+	struct dsi_display *display = (struct dsi_display *) c_conn->display;
+	return display->panel->bl_config.real_bl_level;
 }
 
 static const struct backlight_ops sde_backlight_device_ops = {